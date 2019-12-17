77df006e5e9e5f53bdc5482a56af42117bdc2cd7
Seale Rapolai <seale@avospace.net>
2019-12-17 07:49:36 +0200

    Update Schemas and remove node modules

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,9 +3,55 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
   type Query {
     launches: [Launch]!
-    Launch(id: ID!): Launch
+    launch(id: ID!): Launch
     # Queries for current user
     me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: string
+    mission: Mission
+    rocket: Rocket
+    isBooked: boolean 
+  }
+
+  type Rocket {
+    id: ID!
+    name: string
+    type: string
+  }
+
+  type User {
+    id: ID!
+    email: string!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: string
+    missionPatch(site: PatchSize): string
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrips(launchId: ID!): TripUpdateResponse!
+
+    login(email: string): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: boolean!
+    message: string
+    launches: [Launch]
   }
 `;
 
